cd86b0b8db188d87e45335a88680db7abb2b9ee8
ernst <rfanfan@gmail.com>
2021-02-28 11:22:16 -0500

    ex on some ideas

--- app/src/main/java/com/example/testinggit/MainActivity.java
+++ app/src/main/java/com/example/testinggit/MainActivity.java
@@ -12,5 +12,6 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         System.out.println("Hello");
         System.out.println("GJJKJJSISIKSI");
+        System.out.println("new stuem");
     }
 }
